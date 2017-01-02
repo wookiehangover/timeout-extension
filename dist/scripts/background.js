022ece91e89361fb600ab6676413075445cad234
Sam Breed <sam@quickleft.com>
2017-01-01 18:40:16 -0800

    chore: clean up dist folder

--- dist/scripts/background.js
+++
@@ -1,2 +0,0 @@
-"use strict";chrome.runtime.onInstalled.addListener(function(o){console.log("previousVersion",o.previousVersion)}),chrome.browserAction.setBadgeText({text:"'Allo"}),console.log("'Allo 'Allo! Event Page for Browser Action");
-//# sourceMappingURL=background.js.map
